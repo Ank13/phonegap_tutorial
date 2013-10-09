86ec2f5c2b3a30dc51488889d6a8799923aac8d3
ank13 <avi.kaufman@gmail.com>
2013-10-09 07:48:27 -0400

    Part 3: Using Native Notification

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 var app = {
 
+    // A function named showAlert(). If navigator.notification is available,
+    // use its alert() function. Otherwise, use the default browser alert() function
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
@@ -14,9 +24,16 @@ var app = {
     },
 
     initialize: function() {
-        // this.store = new MemoryStore();
-        this.store = new LocalStorageStore();
+        // Three storage options (memory, HTML5 Local Storage, or WebSql):
+        // this.store = new LocalStorageStore();
         // this.store = new WebSqlStore();
+
+        var self = this;
+        this.store = new MemoryStore(function(){
+          // Testing notification logic when memory store initialized.
+          // Store will call after it has successfully initialized, inoking showAlert
+          self.showAlert('Memory Initialized', 'Info')
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
